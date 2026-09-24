603feac7e91e01b4475b7bb6d0501b24bf1f5435
asjosdokumen-alt <asjosdokumen@gmail.com>
2026-09-24 12:57:27 +0100

    Update i18n.ts

--- src/store/i18n.ts
+++ src/store/i18n.ts
@@ -4,7 +4,7 @@
 import { persistentAtom } from '@nanostores/persistent';
 import { atom } from 'nanostores';
 
-export type Lang = "id" | "jp" | "En";
+export type Lang = "id" | "jp" | "en";
 
 export const langStore = persistentAtom<Lang>("asj_lang", "id", {
   encode: JSON.stringify,
